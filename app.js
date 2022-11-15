fa4a374ad34813d7ca25512269a42c2167b861e5
chachagof <chacha0932@gmail.com>
2022-11-15 19:30:14 +0800

    feat : add detail、edit and delete button add a button back to menu add delete warning

--- app.js
+++ app.js
@@ -40,20 +40,30 @@ app.get('/', (req, res) => {
     .catch(error => console.log(error))
 })
 // detail page
-app.get('/restaurants/:restaurantId', (req, res) => {
-  const restaurantId = req.params.restaurantId
-  const restaurantDescription = restaurants.find(item => {
-    return item.id.toString() === restaurantId
-  })
-  res.render('show', { restaurantDescription })
+app.get('/restaurants/:Id', (req, res) => {
+  const id = req.params.Id
+  return Restaurant.findById(id)
+    .lean()
+    .then(restaurantDescription => { res.render('show', { restaurantDescription }) })
+    .catch(error => console.log(error))
 })
 // search page
 app.get('/search', (req, res) => {
-  const keyword = req.query.keyword
-  const findRestaurant = restaurants.filter(item => {
-    return item.name.toLowerCase().includes(keyword.toLowerCase())
-  })
-  res.render('index', { restaurants: findRestaurant, keyword })
+  const keywords = req.query.keyword
+  const keyword = keywords.trim().toLowerCase()
+  if (keyword === '' || !keyword) {
+    return res.redirect('/')
+  }
+  return Restaurant.find()
+    .lean()
+    .then(restaurants => {
+      const restaurantFilter = restaurants.filter(data => {
+        // 
+        return data.name.toLowerCase().includes(keyword) || data.category.includes(keyword) || data.location.trim().includes(keyword)
+      })
+      return res.render('index', { restaurant: restaurantFilter })
+    })
+    .catch(error => console.log(error))
 })
 // create page
 app.get('/restaurant/new', (req, res) => {
@@ -61,12 +71,48 @@ app.get('/restaurant/new', (req, res) => {
 })
 app.post('/createrRestaurant', (req, res) => {
   const newRestaurant = req.body
-  Restaurant.create(newRestaurant)
+  return Restaurant.create(newRestaurant)
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
 
+// edit page
+app.get('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then(restaurantEdit => res.render('edit', { restaurantEdit }))
+    .catch(error => console.log(error))
+})
 
+app.post('/restaurants/:restaurantId/edit', (req, res) => {
+  const id = req.params.restaurantId
+  const body = req.body
+  return Restaurant.findById(id)
+    .then(restaurantEdit => {
+      restaurantEdit.name = body.name
+      restaurantEdit.name_en = body.name_en
+      restaurantEdit.category = body.category
+      restaurantEdit.image = body.image
+      restaurantEdit.location = body.location
+      restaurantEdit.phone = body.phone
+      restaurantEdit.google_map = body.google_map
+      restaurantEdit.rating = body.rating
+      restaurantEdit.description = body.description
+      return restaurantEdit.save()
+    })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+// delete page 
+app.post('/restaurants/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .then(restaurant => restaurant.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
 
 app.listen(port, () => {
   console.log('Gogogooo')
--- models/restaurant.js
+++ models/restaurant.js
@@ -2,9 +2,6 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const restaurantSchema = new Schema({
-  id: {
-    type: String,
-  },
   name: {
     type: String,
     required: true

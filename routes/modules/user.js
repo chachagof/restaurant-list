9e20cc94e62dc3913e041e14e48f260f5b225daf
chachagof <chacha0932@gmail.com>
2023-02-11 22:44:28 +0800

    feat:add bcrypt and use it

--- routes/modules/user.js
+++ routes/modules/user.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router()
 const User = require('../../models/user')
 const passport = require('passport')
+const bcrypt = require('bcryptjs')
 
+// login
 router.get('/login', (req, res) => {
   res.render('login')
 })
@@ -12,6 +14,7 @@ router.post('/login', passport.authenticate('local', {
   failureRedirect: '/users/login'
 }))
 
+// register
 router.get('/register', (req, res) => {
   res.render('register')
 })
@@ -36,12 +39,20 @@ router.post('/register', (req, res) => {
         return res.render('register',
           { errors, name, email, password, confirmPassword })
       }
-      return User.create({ name, email, password })
-        .then(() => res.redirect('/'))
-        .catch(err => console.log(err))
+      return bcrypt
+        .genSalt(10)
+        .then(salt => bcrypt.hash(password,salt))
+        .then(hash => {
+          User.create({ name, email, password:hash })
+            .then(() => res.redirect('/'))
+            .catch(err => console.log(err))
+        })
+      
+      
     })
 })
 
+// logout
 router.get('/logout', (req, res) => {
   req.logout()
   req.flash('success_msg', '你已經成功登出。')

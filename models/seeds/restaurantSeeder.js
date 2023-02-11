5b74771c127976fde07b5f7aea81564cdb024ad0
chachagof <chacha0932@gmail.com>
2023-02-12 02:03:02 +0800

    modify restaurantSeeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,23 +1,50 @@
-const restaurant = require('../restaurant')
+const Restaurant = require('../restaurant')
+const User = require('../user')
 const restaurantList = require('../../restaurant.json')
 const restaurantSeedInfo = restaurantList.results
+const bcrypt = require('bcryptjs')
 
 const db = require('../../config/mongoose')
 
+
+const SEED_USER = [
+  {
+    name: 'user1',
+    email: 'user1@example.com',
+    password: '12345678',
+    restaurantsIndex: [0, 1, 2]
+  },
+  {
+    name: 'user2',
+    email: 'user2@example.com',
+    password: '12345678',
+    restaurantsIndex: [3, 4, 5]
+  }
+]
+
 db.once('open', () => {
-  restaurantSeedInfo.forEach(element => {
-    restaurant.create({
-      id: `${element.id}`,
-      name: `${element.name}`,
-      name_en: `${element.name_en}`,
-      category: `${element.category}`,
-      image: `${element.image}`,
-      location: `${element.location}`,
-      phone: `${element.phone}`,
-      google_map: `${element.google_map}`,
-      rating: `${element.rating}`,
-      description: `${element.description}`
+  return Promise.all(SEED_USER.map(seeduser => {
+    return bcrypt.genSalt(10)
+      .then(salt => bcrypt.hash(seeduser.password, salt))
+      .then(hash => {
+        return User.create({
+          name: seeduser.name,
+          email: seeduser.email,
+          password: hash
+        })})
+      .then(user => {
+        const userId = user._id
+        const restaurant = []
+        seeduser.restaurantsIndex.map(index => {
+          restaurantSeedInfo[index].userId = userId
+          restaurant.push(restaurantSeedInfo[index])
+        })
+        return Restaurant.insertMany(restaurant)
+      })
+  }))
+    .then(() => {
+      console.log('seeder is finish.')
+      process.exit()
     })
-  })
-  console.log('connection is working')
+    .catch(err => console.log(err))
 })
